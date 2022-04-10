945dd9c15bc1417d24876ab2341f282a1bfd84a1
gptsarthak <62467938+gptsarthak@users.noreply.github.com>
2022-04-10 21:11:25 +0530

    Update main.py - added testing

--- main.py
+++ main.py
@@ -91,3 +91,9 @@ def sigmoid(z):
 
 def sigmoid_prime(z):
     return sigmoid(z)*(1-sigmoid(z))
+
+import mnist_loader
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+training_data = list(training_data)
+net = Network([784, 30, 10])
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
